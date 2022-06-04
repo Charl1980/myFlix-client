d0d2dfcd3b56b276dd72b2b1d9877fd532ba3636
Charl <94119475+Charl1980@users.noreply.github.com>
2022-06-04 13:25:48 +0200

    Create SRC directory with index.html, index.jsx, and index.scss files

---
+++ src/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//IMPORT STATEMENT TO INDICATE THAT YOU NEED TO BUNDLE `./index.scss``
+import './index.scss';
+
+//MAIN COMPONENT - WILL EVENTUALLY USE ALL THE OTHERS
+class MyFlixApplication extends React.Component {
+  render() {
+    return (
+      <div className="my-flix">
+        <div>Good morning</div>
+      </div>
+    );
+  }
+}
+
+//FINDS ROOT OF APP
+const container = document.getElementsByClassName('app-container')[0];
+
+//TELLS React TO RENDER APP IN THE ROOT DOM ELEMENT
+ReactDOM.render(React.createElement(MyFlixApplication), container);
