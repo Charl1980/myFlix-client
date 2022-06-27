93b25d539ec9eac05ff80edc0969325b5f7f6e8a
Charl <94119475+Charl1980@users.noreply.github.com>
2022-06-27 12:02:25 +0200

    React Bootstrap rows, cols and cards added to MovieCard

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Row, Col, CardGroup, Card, Button } from 'react-bootstrap';
 
 export class MovieCard extends React.Component {
   render() {
     const { movie, onMovieClick } = this.props;
     return (
-      <div className="movie-card" onClick={() => onMovieClick(movie)}>{movie.Title}</div>
+      //<div className="movie-card" onClick={() => onMovieClick(movie)}>{movie.Title}</div>
+      <Row>
+        <Col>
+          <CardGroup>
+            <Card bg="light" className="my-3">
+              <Card.Img variant="top" src={movie.ImageURL} />
+              <Card.Body>
+                <Card.Title>{movie.Title}</Card.Title>
+                <Card.Text>{movie.Description}</Card.Text>
+                <Button onClick={() => onMovieClick(movie)} variant="secondary">Open</Button>
+              </Card.Body>
+            </Card>
+          </CardGroup>
+        </Col>
+      </Row>
     );
   }
 }
