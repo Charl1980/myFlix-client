1bf17d9b48971f24ee1a4cbcc718b4dcf2f3d71f
Charl <94119475+Charl1980@users.noreply.github.com>
2022-06-27 11:59:51 +0200

    React Bootstrap rows and cols added to the MainView

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import axios from 'axios';
+//React-Bootstrap Imports
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+//Component Imports
 import { RegistrationView } from '../registration-view/registration-view';
 import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
@@ -59,17 +63,23 @@ export class MainView extends React.Component {
       return <div className="main-view" />;
 
     return (
-      <div className="main-view">
+      <Row className="main-view justify-content-md-center">
         {selectedMovie
-          ? <MovieView movie={selectedMovie}
-            onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+          ? (
+            <Col md={8}>
+              <MovieView movie={selectedMovie}
+                onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+            </Col>
+          )
           : movies.map(movie => (
-            <MovieCard key={movie._id}
-              movie={movie}
-              onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }} />
+            <Col xs={12} sm={6} md={4} lg={3}>
+              <MovieCard key={movie._id}
+                movie={movie}
+                onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }} />
+            </Col>
           ))
         }
-      </div>
+      </Row>
     );
   }
 }
