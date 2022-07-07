d88bf5f5fc4812fdeb439e151e39e46f83186cfb
Charl <94119475+Charl1980@users.noreply.github.com>
2022-07-07 11:46:54 +0200

    Updated 'registration-view'

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,17 +1,67 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import PropTypes from 'prop-types';
 import { Container, Row, Col, Card, CardGroup, Form, Button } from 'react-bootstrap';
+import { propTypes } from 'react-bootstrap/esm/Image';
 
 export function RegistrationView(props) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
   const [birthday, setBirthday] = useState('');
+  const [values, setValues] = useState({
+    usernameErr: '',
+    passwordErr: '',
+    emailErr: ''
+  });
 
-  const handleSubmit = (e) => {
+  const validate = () => {
+    let isReq = true;
+    if (!username) {
+      setValues({ ...values, usernameErr: 'Username required' });
+      isReq = false;
+    } else if (username.length < 6) {
+      setValues({ ...values, usernameErr: 'Username must be at least 6 characters long' });
+      isReq = false;
+    }
+    if (!password) {
+      setValues({ ...values, passwordErr: 'Password required' });
+      isReq = false;
+    } else if (password.length < 8) {
+      setValues({ ...values, passwordErr: 'Password must be at least 8 characters long' });
+      isReq = false;
+    }
+    if (!email) {
+      setValues({ ...values, emailErr: 'Email address required' });
+      isReq = false;
+    } else if (email.indexOf('@') === -1) {
+      setValues({ ...values, emailErr: 'Email is invalid' });
+      isReq = false;
+    }
+    return isReq;
+  }
+
+  const handleRegister = (e) => {
     e.preventDefault();
-    console.log(username, password, email, birthday);
-    props.onRegistration(username);
+    const isReq = validate();
+    if (isReq) {
+      axios.post('https://myflix-movies1980.herokuapp.com/users', {
+        Username: username,
+        Password: password,
+        Email: email,
+        Birthday: birthday
+      })
+        .then(response => {
+          const data = response.data;
+          console.log(data);
+          alert('Registration successful, please login!');
+          window.open('/', '_self');
+        })
+        .catch(response => {
+          console.error(response);
+          alert('Unable to register');
+        });
+    }
   };
 
   return (
@@ -33,6 +83,7 @@ export function RegistrationView(props) {
                       required
                       placeholder="Enter a username"
                     />
+                    {values.usernameErr && <p>{values.usernameErr}</p>}
                   </Form.Group>
 
                   <Form.Group className="mb-3">
@@ -45,6 +96,7 @@ export function RegistrationView(props) {
                       minLength="8"
                       placeholder="Minimum of 8 characters required"
                     />
+                    {values.passwordErr && <p>{values.passwordErr}</p>}
                   </Form.Group>
 
                   <Form.Group className="mb-3">
@@ -56,6 +108,7 @@ export function RegistrationView(props) {
                       required
                       placeholder="Enter a valid email address"
                     />
+                    {values.emailErr && <p>{values.emailErr}</p>}
                   </Form.Group>
 
                   <Form.Group className="mb-3">
@@ -68,7 +121,7 @@ export function RegistrationView(props) {
                     />
                   </Form.Group>
 
-                  <Button variant="dark" type="submit" onClick={handleSubmit}>
+                  <Button variant="dark" type="submit" onClick={handleRegister}>
                     Submit
                   </Button>
 
@@ -83,5 +136,10 @@ export function RegistrationView(props) {
 }
 
 RegistrationView.propTypes = {
-  onRegistration: PropTypes.func.isRequired,
+  register: PropTypes.shape({
+    Username: PropTypes.string.isRequired,
+    Password: PropTypes.string.isRequired,
+    Email: PropTypes.string.isRequired
+  }),
+  //onRegistration: PropTypes.func.isRequired,
 };
