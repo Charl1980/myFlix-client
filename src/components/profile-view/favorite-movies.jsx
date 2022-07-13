456273bca67f573f9e7398a1798edb61897ab8ec
Charl <94119475+Charl1980@users.noreply.github.com>
2022-07-13 17:51:06 +0200

    Corrected a syntax error <Card.Img> and updated the 'remove' button functionality

--- src/components/profile-view/favorite-movies.jsx
+++ src/components/profile-view/favorite-movies.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import axios from 'axios';
 import { Container, Card, Row, Col, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './profile-view.scss';
 
 export function FavoriteMovies(props) {
   const { favoriteMovies, movies } = props;
+  const currentUser = localStorage.getItem('user');
+  const token = localStorage.getItem('token');
+
+  const removeFav = (movieId) => {
+    axios.delete(`https://myflix-movies1980.herokuapp.com/users/${currentUser}/movies/${movieId}`, {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+      .then(() => {
+        alert(`The movie was successfully deleted.`)
+        window.open(`/users/${currentUser}`, '_self');
+      }).
+      catch(error => console.error(error))
+  }
 
   return (
     <Card>
@@ -22,12 +36,12 @@ export function FavoriteMovies(props) {
                 return (
                   <Col xs={12} md={6} lg={3} key={movie._id} className="fav-movie">
                     <Card className="favorites-item card-content">
-                      <Card.Image className="movieCard" variant="top" src={movie.ImageURL} crossOrigin="anonymous" />
+                      <Card.Img className="movieCard" variant="top" src={movie.ImageURL} crossOrigin="anonymous" />
                       <Card.Body>
                         <Card.Title className="movie-card-title">{movie.Title}</Card.Title>
                       </Card.Body>
                     </Card>
-                    <Button variant="secondary" onClick={() => removeFav(movie._id)}>Remove</Button>
+                    <Button variant="secondary" onClick={() => { removeFav(movie._id) }}>Remove</Button>
                   </Col>
                 )
               }
